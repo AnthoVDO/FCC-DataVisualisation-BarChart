f4342d1eb67f690c291c6f08d168108b7c1c3cbe
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-04-28 22:59:47 +0200

    I checked about the API and fetch

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const CHART_WIDTH = 900;
 
 //calling API
 
-const fetchAPI = async(link) => {
-    const response = await fetch(link);
-    return await response;
-}
-
-fetchAPI(URL).then(answer=>console.log(JSON.parse(answer)))
+fetch(URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
 
 
 //using D3
+
+let test = d3.select(".container__chart")
+    .append("svg")
+    .attr("height", CHART_HEIGHT)
+    .attr("width", CHART_WIDTH)
+    .style("background-color", "red")
