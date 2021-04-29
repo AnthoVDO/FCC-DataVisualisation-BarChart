d24d029b749f153afd7add1ea9d0282b3303664e
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-04-29 22:31:58 +0200

    I continued the barChart, I did the fetch and started to creat the graph

--- script.js
+++ script.js
@@ -2,18 +2,52 @@
 const URL = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 const CHART_HEIGHT = 500;
 const CHART_WIDTH = 900;
-
+let dataset = null;
 //calling API
 
-fetch(URL)
-    .then(res => res.json())
-    .then(data => console.log(data))
+const getData = async APIUrl => {
+    const response = await fetch(APIUrl)
+
+    if (response.ok) {
+        const dataUSA = await response.json();
+
+        const svg = d3.select(".container__chart")
+            .append("svg")
+            .attr("height", CHART_HEIGHT)
+            .attr("width", CHART_WIDTH)
+            .style("background-color", "red")
+
+        svg.selectAll("rect")
+            .data(dataUSA.data)
+            .enter()
+            .append("rect")
+            .attr("width", CHART_WIDTH / dataUSA.data.length)
+            .attr("height", d => CHART_HEIGHT - d[1])
+            .attr("x", d => dataUSA.data.indexOf(d))
+            .attr("y", d => CHART_HEIGHT - d[1])
+            .attr("fill", "navy")
+            .attr("class", "bar")
+
+
+
+
+    } else {
+        console.log("fetch failed : ", response.status)
+    }
+}
+
+getData(URL);
 
 
 //using D3
-
-let test = d3.select(".container__chart")
+/*
+const svg = d3.select(".container__chart")
     .append("svg")
     .attr("height", CHART_HEIGHT)
     .attr("width", CHART_WIDTH)
     .style("background-color", "red")
+
+svg.selectAll("circle")
+    .append()
+
+    */
